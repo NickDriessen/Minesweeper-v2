238268f9ac06531881516d36f645f803cd19f727
TheEarthsMoon <nick.driessen@hotmail.com>
2025-08-25 15:05:30 +0200

    Add files via upload

---
+++ header.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+int make_board(int board_option, int real_board[15][15], int bombs);
+int print_board(int board_option, int visual_board[15][15]);
+int ask_reveal(int board_option, int visual_board[15][15], int real_board[15][15]);
+
+enum {SMALL = 5, MEDIUM = 8, LARGE = 15};
+enum {SAFE, BOMB = -1};
+
+#endif
